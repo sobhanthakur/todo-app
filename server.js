a52f159909c30b39cdc16392b96c1f5618ef1f7c
sobhanthakur <sobhant@mindfiresolutions.com>
2020-08-24 17:02:08 +0530

    Production Build ready

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 
 const app = express();
 
+const path = require("path");
+
 // Connect to DB
-connectDB()
+connectDB();
 
 // Init middleware
 // This helps to pass the request body to the controllers
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./api/controllers/users'));
-app.use('/api/todo', require('./api/controllers/todo'));
+app.use("/api/users", require("./api/controllers/users"));
+app.use("/api/todo", require("./api/controllers/todo"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
